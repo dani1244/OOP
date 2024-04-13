5ee4811e78a8bc266f06b38b55589d8675815c55
dani1244 <121317384+dani1244@users.noreply.github.com>
2024-04-13 16:51:25 +0100

    bvbb

--- lesson2/notasPT.java
+++ lesson2/notasPT.java
@@ -1,8 +1,38 @@
 package lesson2;
 
+import java.util.Scanner;
+
 public class notasPT {
-    public static void main(String [] args){
-        
-    }
     
+        public static int validNota(Scanner sc){
+        int notaT;
+        System.out.println("Insira a nota da teória!");
+        int notaP;
+        System.out.println("Insira a nota da prática!");
+
+         while(true){
+            notaT = sc.nextInt();
+            notaP = sc.nextInt();
+            if ((notaT >= 0) && (notaP<=20) || (notaT<=20) && (notaP>=0)){
+                //Se for verdade, pode prosseguir
+                break;
+            }else {
+                System.out.println("Nota Inválida!");
+            }
+        }
+         return notaT;
+         return notaP;
+    }
+        public static void main(String []  args){
+        Scanner sc = new Scanner(System.in);
+        int notaT = validNota(sc);
+        double notaFinal = (0.4*notaT)+(0.6*notaP);
+        if (notaP<7||notaT<7){
+            System.out.println("66");
+            System.out.println(" A nota final foi de : " +notaFinal);
+        }
+
 }
+        
+    
+
